6a5e1afb69f6f2b100bea75da9eacbc0db9a692e
Bruna Terribile <brunaterribile@gmail.com>
2022-10-23 17:25:51 -0300

    Barra progresso botão Hoje

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,31 @@
 import styled from "styled-components"
 import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 
-export default function Menu() {
-    
+export default function Menu(props) {
+    const navigate = useNavigate()
+    const percentage = 66;
+
     return (
         <BottomBar>
             <StyledLink to={'/habitos'}><h1>Hábitos</h1></StyledLink>
-            <div><StyledLink to={'/hoje'}>Hoje</StyledLink></div>
+                
+            <div onClick={() => navigate('/hoje')} className="progress" >
+                <CircularProgressbar value={percentage} text={`Hoje`} 
+                    styles={buildStyles({pathColor: `white`, 
+                                        textColor: `white`,
+                                        width: `100px`,
+                                        heigth: `100px`,
+                                        backgroundColor: "#52B6FF",
+                                        pathColor: "#fff",
+                                        trailColor: "transparent"})}
+                                        background
+                                        backgroundPadding={6}
+                /> </div>
+
             <StyledLink to={'/historico'}><h1>Histórico</h1></StyledLink>
         </BottomBar>
     )
@@ -18,7 +36,6 @@ const BottomBar = styled.div`
     height: 70px;
     position: fixed;
     bottom: 0;
-    background-color: pink;
     display: flex;
     align-items: center;
     justify-content: space-around;
@@ -26,21 +43,21 @@ const BottomBar = styled.div`
     font-size: 18px;
     z-index: 2;
     text-decoration: none;
+    border: 1px solid grey;
 
-    div {
+    .progress {
+        z-index: 3;
+        width: 91px;
+        height: 91px;
         display: flex;
         align-items: center;
         justify-content: center;
         background-color: #52B6FF;
-        color: red;
-        width: 91px;
-        height: 91px;
         border-radius: 50px;
         position: absolute;
         bottom: 15px;
     }
 `
-
 const StyledLink = styled(Link)`
     text-decoration: none;
     color: white;
